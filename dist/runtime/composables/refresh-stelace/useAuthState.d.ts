a80ffc31233480be2f0a77422e1237435ca0737f
Philipp Fauser <dev@philippfauser.com>
2024-01-11 19:02:38 +0100

    with last

---
+++ dist/runtime/composables/refresh-stelace/useAuthState.d.ts
@@ -0,0 +1,13 @@
+import { ComputedRef } from "vue";
+import { CookieRef } from "#app";
+import { useAuthState as useRefreshAuthState } from "../refresh/useAuthState";
+import { SessionStatus } from "../../types";
+type UseAuthStateReturn = ReturnType<typeof useRefreshAuthState> & {
+    rawStelaceToken: CookieRef<string | null>;
+    stelaceToken: ComputedRef<string | null>;
+    rawStelaceRefreshToken: CookieRef<string | null>;
+    stelaceRefreshToken: ComputedRef<string | null>;
+    stelaceStatus: ComputedRef<SessionStatus>;
+};
+export declare const useAuthState: () => UseAuthStateReturn;
+export default useAuthState;
